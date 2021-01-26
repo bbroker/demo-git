c1f062a772016aa35044cb19b576fa5470e2a61f
ThanhNguyen <thanhnguyen212.2804@gmail.com>
2021-01-26 20:50:16 +0700

    first use Github

--- src/Pig.js
+++ src/Pig.js
@@ -1,10 +1,15 @@
-function Pig (name) {
+function Pig (name, leg) {
     this.name= name;
     this.stomach = [];
+    this.leg = leg;
 };
 
 Pig.prototype.eat = function (bran) {
     this.stomach.push(bran);
 };
+
+Pig.prototype.run = function () {
+    console.log('run');
+}
 
 module.exports = Pig;
