ea246878063d11ffb7afbee218779af1c3c03386
ThanhNguyen <thanhnguyen212.2804@gmail.com>
2021-01-26 20:53:50 +0700

    details chicken

--- src/Chicken.js
+++ src/Chicken.js
@@ -1,7 +1,12 @@
 
 const Chicken = () => {
 
-    return <div>Chicken</div>
+    return <div>
+            <h1>Chicken</h1>
+            <div>dish: fried chicken</div>
+            <div>number of people: 4</div>
+            <div>at: restautrent</div>
+        </div>
 };
 
 export default Chicken;
